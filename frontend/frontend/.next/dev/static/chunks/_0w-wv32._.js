154eea730e61dfbf6c705c9abf059381c01b94b8
Julian Vanegas López <julianvanegaslopez@gmail.com>
2026-09-16 00:35:39 -0500

    Proyecto de Banco con Frontend y Backend

--- frontend/frontend/.next/dev/static/chunks/_0w-wv32._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/node_modules_0pxfn6l._.js","static/chunks/src_app_providers_tsx_195mga_._.js","static/chunks/node_modules_0y3ve9_._.js","static/chunks/src_1x29bfa._.js"],
-    source: "entry"
-});
